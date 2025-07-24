26179dc568db2f5a9b7c31705266f1ade7382ac3
nuuuwan <nuuuwan@gmail.com>
2025-07-24 13:44:00 +0530

    usage print

--- workflows/from_generic_file.py
+++ workflows/from_generic_file.py
@@ -7,6 +7,9 @@ from tts import Speech
 # .\tests\input.docx.mp3
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: tts <input_file_path> <output_file_path>")
+        sys.exit(1)
     file_path = sys.argv[1]
     output_path = sys.argv[2]
     Speech.from_generic_file(file_path).write(output_path)
